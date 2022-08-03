6b98e3fd6b9ce402b09eba3eb31e17fd518acde4
Bharat Sharma <82795831+Bharatsharma19@users.noreply.github.com>
2022-08-03 21:52:27 +0530

    03 Aug 2022

--- routes/purchase.js
+++ routes/purchase.js
@@ -5,10 +5,34 @@ router.get("/", function (req, res) {
   res.render("product", { title: "Products" });
 });
 
-router.get("/checkout/get_amt", function (req, res) {
-  console.log("Request ", req);
+router.get("/checkout", function (req, res) {
+  console.log("Request ", req.query);
 
-  res.render("product");
+  var pn = req.query.productName;
+  var qty = req.query.qty;
+  var rate = 0;
+
+  if (pn == "Suit") {
+    rate = 999;
+  } else if (pn == "T-Shirt") {
+    rate = 699;
+  } else if (pn == "Jeans") {
+    rate = 1599;
+  } else if (pn == "Suit") {
+    rate = 3999;
+  } else if (pn == " ") {
+    var error = "Please select a product";
+  }
+
+  var amt = rate * qty;
+
+  res.render("product", {
+    productName: pn,
+    error: error,
+    qty: qty,
+    rate: rate,
+    amount: amt,
+  });
 });
 
 module.exports = router;
